0a74ff8500d7a4427cf6d2c2a042d5423c1c40b0
Sword <fanmeijian@gmail.com>
2024-09-10 20:27:48 +0800

    remove debug string

--- src/test/java/eu/nimble/indexing/IndexingAppTests.java
+++ src/test/java/eu/nimble/indexing/IndexingAppTests.java
@@ -1,21 +1,41 @@
 package eu.nimble.indexing;
 
 import eu.nimble.indexing.service.OntologyService;
+import eu.nimble.indexing.service.impl.OntologyServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableJpaRepositories
 public class IndexingAppTests {
 	@Autowired
 	private OntologyService service;
 
 	@Test
 	public void contextLoads() {
-		this.service.upload(null, null, null);
+		Path filePath = Paths.get("/Users/fansword/Downloads/eclass_514en.owl");
+
+		try {
+			byte[] fileBytes = Files.readAllBytes(filePath);
+			String content = new String(fileBytes);
+			this.service.upload("application/rdf+xml", Collections.singletonList("http://www.ebusiness-unibw.org/ontologies/eclass/5.1.4/#"), content);
+
+			System.out.println(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
