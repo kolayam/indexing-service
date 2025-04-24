521094bff5c9e60772d403f03c82e18b06ff49f5
Sword <fanmeijian@aliyun.com>
2025-04-25 01:29:52 +0800

    remove log

--- src/main/java/eu/nimble/indexing/service/impl/PropertyServiceImpl.java
+++ src/main/java/eu/nimble/indexing/service/impl/PropertyServiceImpl.java
@@ -67,7 +67,6 @@ public class PropertyServiceImpl extends SolrServiceImpl<PropertyType> implement
 		logger.info("classTypes: " + String.join("", classTypes));
 		try {
 			List<PropertyType> result = propRepo.findByProductIn(classTypes);
-			logger.info(new ObjectMapper().writeValueAsString(result));
 			return new SearchResult<PropertyType>(result);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
